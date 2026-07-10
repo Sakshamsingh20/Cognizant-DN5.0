b11191ddae78feb721791e91bec62ad622bb01b7
Saksham Singh <sakshamsingh20aug2005@gmail.com>
2026-07-10 09:37:34 +0530

    Initial commit

--- src/main/java/com/cognizant/week3/SpringRest/SpringRestApp.java
+++ src/main/java/com/cognizant/week3/SpringRest/SpringRestApp.java
@@ -2,8 +2,14 @@ package com.cognizant.week3.SpringRest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+    "com.cognizant.week3.SpringRest",
+    "com.cognizant.week4.microservices.accountservice",
+    "com.cognizant.week4.microservices.loanservice"
+})
 public class SpringRestApp {
     public static void main(String[] args) {
         SpringApplication.run(SpringRestApp.class, args);
